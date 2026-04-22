0e92c196189f89e76428ab247dac6314335b799c
NiJingzhe <1448976235@qq.com>
2026-04-22 16:56:48 +0800

    Update formula for TermPDF 0.1.1

--- Formula/termpdf.rb
+++ Formula/termpdf.rb
@@ -1,36 +1,20 @@
 class Termpdf < Formula
   desc "Terminal PDF reader built with Rust, ratatui, PDFium, and the kitty image protocol"
   homepage "https://github.com/NiJingzhe/TermPDF"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/NiJingzhe/TermPDF/releases/download/v0.1.0/termpdf-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ee705e62c7c3bea33e8be1fe44db71b8386e39f4af5b6e9ce586a20be0c960f6"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://github.com/NiJingzhe/TermPDF/releases/download/v0.1.0/termpdf-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "17245392c2204a58e51ea4301d18af410b655a576068e9cbfc1b8c3229ca9bbe"
-    end
-
-    on_arm do
-      url "https://github.com/NiJingzhe/TermPDF/releases/download/v0.1.0/termpdf-0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "21865e449f661d1e0d1755e07c5843a87f7add41ae29c33bfbdc53f287053fa9"
+      url "https://github.com/NiJingzhe/TermPDF/releases/download/v0.1.1/termpdf-0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "684de354e1e5e1e6b6a347030b89b7dd0edfe49cbed8fabb13e841b3d661b2d4"
     end
   end
 
   def install
     libexec.install "termpdf"
 
-    if OS.mac?
-      libexec.install "libpdfium.dylib"
-    else
-      libexec.install "libpdfium.so"
-    end
+    libexec.install "libpdfium.dylib"
 
     (bin/"termpdf").write <<~EOS
       #!/bin/bash
