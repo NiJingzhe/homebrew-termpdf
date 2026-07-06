b57d6dc61738d4609d71b32e62f898ffc12904a6
NiJingzhe <1448976235@qq.com>
2026-07-06 21:33:58 +0800

    feat: bump version to 0.3.0

--- Formula/termpdf.rb
+++ Formula/termpdf.rb
@@ -1,5 +1,5 @@
 class Termpdf < Formula
-  TERMPDF_VERSION = "0.2.0"
+  TERMPDF_VERSION = "0.3.0"
 
   desc "Terminal PDF reader built with Rust, ratatui, PDFium, and the kitty image protocol"
   homepage "https://github.com/NiJingzhe/TermPDF"
@@ -9,7 +9,7 @@ class Termpdf < Formula
   on_macos do
     on_arm do
       url "https://github.com/NiJingzhe/TermPDF/releases/download/v#{TERMPDF_VERSION}/termpdf-#{TERMPDF_VERSION}-aarch64-apple-darwin.tar.gz"
-      sha256 "283f77f49ccd5ba088d95cb2a5e2aef571dddaf7d87714f38f9479c5d12658b0"
+      sha256 "00235f958cba6b71732b364bed02f87aaac0fbc080175775af2f1392babae195"
     end
   end
 
